e73c35162a036bcd2716b76f857d6d146c9a8fcf
Ratta Chindasilpa <70811729+Ratta-Chindasilpa@users.noreply.github.com>
2020-09-19 23:03:39 +0700

    Create Lecture105_Ratta_C.py

---
+++ assignment/Lecture105_Ratta_C.py
@@ -0,0 +1,32 @@
+class Vehicle:
+    lecenseCode = ""
+    serialCode = ""
+    face = ""
+    def turnOnAirConditioner(self):
+        print("Turn On : Air")
+
+class Car(Vehicle): #ใส่classที่จะสืบทอด #เป็น class ลูก
+    def sayHello(self):
+        print("Hello World!")
+
+class Pickup(Vehicle):
+    pass
+
+class Van(Vehicle):
+    pass
+
+class EstateCar(Vehicle):
+    pass
+
+pickUp1 = Pickup()
+pickUp1.turnOnAirConditioner()
+
+car1 = Car()
+car1.turnOnAirConditioner()
+car1.sayHello()
+
+van1 = Van()
+van1.turnOnAirConditioner()
+
+estatecar1 = EstateCar()
+estatecar1.turnOnAirConditioner()
